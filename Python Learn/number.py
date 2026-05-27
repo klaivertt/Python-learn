d3c54dddb531bdcfcdf606407622884b9e902330
Klaivert <69586612+klaivertt@users.noreply.github.com>
2026-05-27 22:38:04 +0200

    Update number.py

--- Python Learn/number.py
+++ Python Learn/number.py
@@ -1,12 +1,13 @@
-def get_int():
+def GetInt(_str):
 #Ask to user an integer if the result isn't an integer retry the while
     while True :  
         try :
-            return int(input("What's x ? "))
+            return int(input(_str))
         except ValueError:
-            print("x is not an integer")
+            # print("x is not an integer")
+            pass
 
 def main():
-    print(f"x is {get_int()}")
+    print(f"x is {GetInt("What's x ? ")}")
             
 main()
