ab343ad2f2d1907a8bfad8f6e87771baf8d9d8e8
Klaivert <69586612+klaivertt@users.noreply.github.com>
2026-05-30 16:00:02 +0200

    Update calculator.py

--- Python Learn/calculator.py
+++ Python Learn/calculator.py
@@ -6,6 +6,13 @@ name = input("choise one opération between, Add, Sub, Mult, Div,Mod")
          case "Add" | "Sub" |"Mult"|"Div" |"Mod":
             return name
         case _:
+      
+def GetInt(_prompt):
+   while True :  
+        try :
+            return int(input(_prompt))
+        except ValueError:
+            pass
 
 def Add(_a,_b):
    return _a + _b
@@ -23,6 +30,24 @@ def Mod(_a,_b):
    return _a % _b
 
 def main():
+    x = GetInt("Choise X")
+    y = GetInt("Choise Y")
     
-    
+   choisse = Choisse()
+   result = 0
+   match choisse:
+         case "Add":
+         result = Add(x,y)
+         case "Sub":
+          result = Sub(x,y)
+         case "Mult":
+          result = Mult(x,y)
+         case "Div":
+          result = Div(x,y)
+         case "Mod":
+          result = Mod(x,y)
+        case _:
+   
+   print(f"The result of your calcul is {result}")
+   
 main()
