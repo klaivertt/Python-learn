5a900989bd2f30950b6b7b72381d2b6317814dc3
Klaivert <69586612+klaivertt@users.noreply.github.com>
2026-05-30 15:48:59 +0200

    Update calculator.py

--- Python Learn/calculator.py
+++ Python Learn/calculator.py
@@ -1,5 +1,11 @@
 def Choisse()
-    
+name = input("choise one opération between, Add, Sub, Mult, Div,Mod")
+    while True
+      name = input("choise one opération ?")
+      match name:
+         case "Add" | "Sub" |"Mult"|"Div" |"Mod":
+            return name
+        case _:
 
 def Add(_a,_b):
    return _a + _b
@@ -7,7 +13,7 @@ def Add(_a,_b):
 def Sub(_a,_b):
    return _a - _b
 
-def mult(_a,_b):
+def Mult(_a,_b):
    return _a * _b
 
 def Div(_a,_b):
