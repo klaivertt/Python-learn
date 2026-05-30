e3289ca8852857dedc6f4bb09ffb1c759e872773
Klaivert <69586612+klaivertt@users.noreply.github.com>
2026-05-30 15:39:41 +0200

    Update calculator.py

---
+++ Python Learn/calculator.py
@@ -0,0 +1,22 @@
+def Choisse()
+    
+
+def Add(_a,_b):
+   return _a + _b
+
+def Sub(_a,_b):
+   return _a - _b
+
+def mult(_a,_b):
+   return _a * _b
+
+def Div(_a,_b):
+   return _a / _b
+
+def Mod(_a,_b):
+   return _a % _b
+
+def main():
+    
+    
+main()
