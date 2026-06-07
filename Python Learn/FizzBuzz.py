c720206f88f652b1137319f80dadde98af5bc7bd
Klaivert <69586612+klaivertt@users.noreply.github.com>
2026-06-07 16:09:45 +0200

    Update FizzBuzz.py

--- Python Learn/FizzBuzz.py
+++ Python Learn/FizzBuzz.py
@@ -1,23 +1,15 @@
-def GetInt(_prompt):
-    while True :
-        try :
-            return int(input(_prompt))
-        except ValueError:
-            pass
-
-
-
-
 def main() : 
-    numb = GetInt("Choise an number : ")
-    
-    result = ""
-    if (numb % 3 == 0):
-        result += "Fizz"
+   
+    for i in range(1, 101):
+        result = ""
+        if (i % 3 == 0):
+            result += "Fizz"
+        if (i % 5 == 0):
+            result += "Buzz"
         
-    if (numb%5 == 0):
-        result += "Buzz"
+        if (result == "") :
+            result = str(i) 
+        print(result)  
         
-    print(result)  
 
 main()
