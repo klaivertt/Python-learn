f27d2c18d56741f68cf1eeb8ac8053d9296c19c0
Klaivert <69586612+klaivertt@users.noreply.github.com>
2026-06-08 14:02:56 +0200

    Update Password.py

--- Python Learn/Password.py
+++ Python Learn/Password.py
@@ -1,5 +1,37 @@
 def main():
+    password = ""
     while True:
+        password = input("choise a password : ")
         
+        print("you choise :" + password + "as password")
+        if len(password) < 8 :
+            print("password to short")
+        else : 
+            print("your password have the required length")
+            
+            capitalised = False 
+            lower = False 
+            haveANumber = False
+            
+            for i in range(len(password)):
+                
+                if(password[i].islower()):
+                    lower = True
+                if(password[i].isupper()):
+                    capitalised = True
+                    lower = True
+                if(password[i].isdigit()):
+                    haveANumber = True
+                                
+                if capitalised and lower and haveANumber:
+                    break
+                
+                
+            if(capitalised and lower and haveANumber) :
+                print("your password have one capitalised letter and lower letter and a number")
+                break
+            else :
+                 print("your password miss one capitalised letter or lower letter or a number")
+    print("your password is availlable")
 
 main()
