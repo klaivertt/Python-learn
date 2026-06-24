19eca0dfdf38531e89086b8542656aa12442c5ae
Klaivert <69586612+klaivertt@users.noreply.github.com>
2026-06-24 20:36:34 +0200

    Create RockPaperCisor.py

---
+++ Python Learn/RockPaperCisor.py
@@ -0,0 +1,97 @@
+import random
+
+class Player:
+    
+    def __init__(self):
+        self.choice = ""
+        
+    
+    def Choice(self):
+        while True:
+            self.choice = input("Choose between, 'Rock' | 'Paper' | 'Cisor' : ").strip().lower()
+            
+            if self.choice in ['rock', 'paper', 'cisor']:                
+                break 
+            else :
+                print("Please enter an valid value between, 'Rock' | 'Paper' | 'Cisor' ")
+        
+    def GetChoice(self):
+        return self.choice
+    
+class Robot:
+    
+    def __init__(self):
+        self.choice = ""
+        self.playerChoice = {"rock" : 0, "paper" : 0, "cisor" : 0}
+        
+    
+    def Choice(self):
+        total = self.playerChoice["rock"] + self.playerChoice["paper"] + self.playerChoice["cisor"] 
+
+        if total == 0:
+            self.choice = random.choice(["rock", "paper", "cisor"])
+        else:
+            weight = [self.playerChoice["rock"] / total, self.playerChoice["paper"] / total, self.playerChoice["cisor"] / total ]
+            self.choice= random.choice(["rock", "paper", "cisor"], weights= weight)
+    
+    def Analyse(self, _choice):
+        self.playerChoice[_choice] += 1
+        
+    def GetChoice(self):
+        return self.choice
+        
+def isVictory(_choice1, _choice2):
+    victory = False
+    
+    if _choice1 == "paper" and _choice2 == "cisor":
+        print("Robot win")
+        victory = True
+    elif  _choice1 == "cisor" and _choice2 == "paper":
+        print("Player win")
+        victory = True
+        
+    elif _choice1 == "rock" and _choice2 == "paper":
+        print("Robot Win")
+        victory = True
+    elif _choice1 == "paper" and _choice2 == "rock":
+        print("Player Win")
+        victory = True
+        
+    elif  _choice1 == "cisor" and _choice2 == "rock":
+        print("Robot Win")
+        victory = True
+    elif  _choice1 == "rock" and _choice2 == "cisor":
+        print("Player Win")
+        victory = True
+        
+    else :
+        print('Draw')
+    
+    return victory
+    
+
+def main():
+    gameWin = False 
+    
+    player = Player() 
+    robot =  Robot()
+    
+    nbRound = 0
+    
+    while not gameWin:
+        player.Choice()
+        robot.Choice()
+        print(f"Player choice : {player.GetChoice()} , Robot choice : {robot.GetChoice()}")
+        robot.Analyse(player.GetChoice())
+        
+        nbRound +=1
+        
+        gameWin = isVictory(player.GetChoice(), robot.GetChoice())
+    
+    print(f"Game finished in {nbRound} rounds")
+          
+        
+        
+
+if __name__ == "__main__":
+    main()
